9d68ae2ae94d3436765d0fd774402c9acf5106d0
younishamrh10 <yoha06001@utb.vaxjo.se>
2023-10-10 22:22:24 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Inlämningsuppgift3._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Har du gått ut gymnasiet?");
+            Console.WriteLine("j = Ja, n = Nej")
+            string svar = Console.ReadLine().ToLower();
+            Console.WriteLine("Hur gammal är du?");
+            int ålder = int.Parse(Console.ReadLine());
+        }
+    }
+}
